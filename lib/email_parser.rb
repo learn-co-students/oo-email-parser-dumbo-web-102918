92e6d824461cc7af03fd9431990b0f9ed3325063
La Fountain <github email address>
2018-11-03 16:54:53 -0400

    Complete oo-email-parser

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,4 +1,28 @@
-# Build a class EmailParser that accepts a string of unformatted 
+# Build a class EmailParser that accepts a string of unformatted
 # emails. The parse method on the class should separate them into
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
+
+require "pry"
+require "byebug"
+
+class EmailParser
+  attr_reader :email
+
+  def initialize(string)
+    @email = string
+  end
+
+  def parse
+    # byebug
+    if self.email.include?(",")
+      temp = self.email.delete(",").split(" ")
+    else
+      temp = self.email.split(" ")
+    end
+    result = []
+    temp.each{|email|  result << email unless result.include?(email)}
+    result
+  end
+
+end
